a1a6d797be2db8afd47872790eade97b5e86356c
정호연 <jea7730@gmail.com>
2024-06-12 12:54:08 +0900

    todo list 완료

--- template.js
+++ template.js
@@ -6,15 +6,26 @@ const tagComponent = (tagName, textNode) => {
 };
 
 const container = () => {
+  // return todayDate;
+
+  const checkDate = (today) => {
+    if (today === date) {
+      return `${tagComponent('div', `정답! 오늘은 6월 ${date}일 입니다.`)}
+  ${tagComponent('div', `TIM : 6월 12일은 호연이의 탄생일!`)}
+      `;
+    } else {
+      return `${tagComponent('div', `땡! 오늘은 6월 ${date}일 입니다.`)}`;
+    }
+  };
+
   return `
-  ${tagComponent('header', '헤더부분')}
-  ${tagComponent('main', '메인부분')}
-  ${tagComponent('footer', '푸터부분')}
+  ${tagComponent('div', '오늘은 6월 ?일 일까요?')}
+  ${checkDate(13)}
   `;
 };
 
 console.log(container());
-//todo-1 날짜 함수 구하기
-//todo-2 함수에 적용
-//todo-3 12일(오늘)이 아니라면 A방식
-//todo-4 12일(오늘)이 맞다면 B방식
+//todo-1 날짜 함수 구하기 _ 완료
+//todo-2 함수에 적용 _ 완료
+//todo-3 12일(오늘)이 아니라면 A방식 _ 완료
+//todo-4 12일(오늘)이 맞다면 B방식 _ 완료
