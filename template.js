d79a69ab7d7af95e932b72233540ceaff137ac31
정호연 <jea7730@gmail.com>
2024-06-12 12:27:08 +0900

    date 모듈 불러오기_완료

--- template.js
+++ template.js
@@ -1,3 +1,6 @@
+const date = require('./date.js');
+// console.log(date);
+
 const tagComponent = (tagName, textNode) => {
   return `<${tagName}>${textNode}</${tagName}>`;
 };
@@ -10,6 +13,7 @@ const container = () => {
   `;
 };
 
+console.log(container());
 //todo-1 날짜 함수 구하기
 //todo-2 함수에 적용
 //todo-3 12일(오늘)이 아니라면 A방식
