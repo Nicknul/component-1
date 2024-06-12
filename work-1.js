eda6fcc396977675ae67600e4e35ceaecea37ea3
정호연 <jea7730@gmail.com>
2024-06-12 10:44:54 +0900

    기존 틀을 함수로 만들어 호출하는 방식

---
+++ work-1.js
@@ -0,0 +1,9 @@
+const basicData = () => {
+  return { name: '정호연', age: 27, job: '백수' };
+};
+
+const component = () => {
+  let obj = basicData();
+  const decision = () => {};
+  return `<html></html>`;
+};
